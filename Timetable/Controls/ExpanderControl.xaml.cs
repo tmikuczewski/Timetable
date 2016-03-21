5560c6a760518d0e35c46ba52d79e3200bf3bb5a
Tomasz <miktomi15@gmail.com>
2016-03-21 08:26:08 +0100

    Resolved #1 Dodano brakujące pliki kontrolek.

---
+++ Timetable/Controls/ExpanderControl.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System.Windows.Controls;
+
+using Timetable.Utilities.Enums;
+
+namespace Timetable.Controls
+{
+	/// <summary>
+	/// Interaction logic for ExpanderControl.xaml
+	/// </summary>
+	public partial class ExpanderControl : UserControl
+	{
+		#region Constructors
+
+		public ExpanderControl(string text, ExpanderControlImgType ect)
+		{
+			InitializeComponent();
+
+			this.button.Content = text;
+
+			switch (ect)
+			{
+				case ExpanderControlImgType.ADD_BTN:
+					this.image.Source = Utilities.Utilities.ConvertBitmapToBitmapImage(Properties.Resources.plus);
+					break;
+				case ExpanderControlImgType.CHANGE_BTN:
+					this.image.Source = Utilities.Utilities.ConvertBitmapToBitmapImage(Properties.Resources.pen);
+					break;
+				case ExpanderControlImgType.REMOVE_BTN:
+				default:
+					this.image.Source = Utilities.Utilities.ConvertBitmapToBitmapImage(Properties.Resources.recycleBin);
+					break;
+			}
+		}
+
+		#endregion
+
+		#region Overridden methods
+
+		#endregion
+
+		#region Public methods
+
+		#endregion
+
+		#region Properties
+
+		#endregion
+
+		#region Private methods
+
+		#endregion
+
+		#region Events
+
+		#endregion
+
+		#region Constants and Statics
+
+		#endregion
+
+		#region Fields
+
+		#endregion
+	}
+}
---
+++ Timetable/Controls/PersonControl.xaml.cs
@@ -0,0 +1,49 @@
+﻿namespace Timetable.Controls
+{
+	/// <summary>
+	/// Interaction logic for PersonControl.xaml
+	/// </summary>
+	public partial class PersonControl : System.Windows.Controls.UserControl
+	{
+		#region Constructors
+
+		public PersonControl(string pesel, string firstName, string lastName)
+		{
+			InitializeComponent();
+
+			this.textBlockPesel.Text = pesel;
+			this.textBlockFirstName.Text = firstName;
+			this.textBlockLastName.Text = lastName;
+		}
+
+		#endregion
+
+		#region Overridden methods
+
+		#endregion
+
+		#region Public methods
+
+		#endregion
+
+		#region Properties
+
+		#endregion
+
+		#region Private methods
+
+		#endregion
+
+		#region Events
+
+		#endregion
+
+		#region Constants and Statics
+
+		#endregion
+
+		#region Fields
+
+		#endregion
+	}
+}
