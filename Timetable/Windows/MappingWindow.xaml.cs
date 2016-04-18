de455a10bd0da062f18c0179667b60d42c855f90
Tomasz <miktomi15@gmail.com>
2016-04-18 18:08:07 +0200

    Task 4 Dodano: - (częściowo) okno MappingWindow.

---
+++ Timetable/Windows/MappingWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace Timetable.Windows
+{
+	/// <summary>
+	/// Interaction logic for MappingWindow.xaml</summary>
+	public partial class MappingWindow : System.Windows.Window
+	{
+		#region Constructors
+
+		#endregion
+
+		#region Overridden methods
+
+		#endregion
+
+		#region Public methods
+
+		#endregion
+
+		#region Properties
+
+		#endregion
+
+		#region Private methods
+
+		#endregion
+
+		#region Constants and Statics
+
+		#endregion
+
+		#region Fields
+
+		#endregion
+	}
+}
